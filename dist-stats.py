6111ee5e35d29ad926716833deb69c1605c65295
mioppong <michaeloppong123@gmail.com>
2019-10-13 23:47:17 -0400

    finished part b of this assignment

--- dist-stats.py
+++ dist-stats.py
@@ -1,4 +1,4 @@
-import csv
+import matplotlib.pyplot as plt; plt.rcdefaults()
 import sys
 import pandas as pd
 import numpy as np
@@ -7,6 +7,7 @@ import argparse
 from pprint import pprint
 import collections
 import operator
+import matplotlib.pyplot as plt
 
 
 def restaurant_category_dist(filename, city):
@@ -80,6 +81,46 @@ def restaurant_review_dist(filename,city):
     for categories,occurences in sorted_answer[::-1]:
         print(categories,":",occurences,":",dict_for_stars[categories])
     
+def chart(filename, city):
+    df = pd.read_csv(filename)
+    list_of_all_types_of_categories = []
+
+    
+    for index,row in df.iterrows():
+       if (("Restaurants" not in row.categories) or (row.city != city)):
+            df.drop(index,inplace=True)
+      
+    #splits all subcategories and put 
+    #all into one list
+    for string in list(df.categories):
+        for s in string.split(';'):
+            list_of_all_types_of_categories.append(s)
+
+    while "Restaurants" in list_of_all_types_of_categories:
+        list_of_all_types_of_categories.remove("Restaurants") 
+
+    while "Food" in list_of_all_types_of_categories:
+        list_of_all_types_of_categories.remove("Food") 
+    
+
+    dictionary_of_everything = {}
+    for l in list_of_all_types_of_categories:
+        dictionary_of_everything[l] = list_of_all_types_of_categories.count(l)
+    
+    sorted_answer = (sorted(dictionary_of_everything.items(),key=operator.itemgetter(1)))
+
+    x = list(dictionary_of_everything.keys())
+
+    y = list(dictionary_of_everything.values())
+    
+
+    plt.figure(figsize=(20,3))
+    plt.bar(range(len(dictionary_of_everything)), dictionary_of_everything.values(), align="edge", width=0.1)
+    plt.xticks(np.arange(len(x)),x,fontsize=2)
+
+    plt.show()
+
+
 
 def command_line_args():
     parser = argparse.ArgumentParser()
@@ -89,8 +130,9 @@ def command_line_args():
     filename = args.filename
     city = args.city
 
-    #restaurant_category_dist(filename,city)
-    #restaurant_review_dist(filename,city)
+    restaurant_category_dist(filename,city)
+    restaurant_review_dist(filename,city)
+    chart(filename, city)
 
 def main():
         command_line_args()
